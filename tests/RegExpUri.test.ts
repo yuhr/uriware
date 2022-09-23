70e174dfa135161051b49dd4c04205ddc49bded8
Yu Shimura <mail@yuhr.org>
2022-09-23 17:14:14 +0900

    test: add a few tests

--- tests/RegExpUri.test.ts
+++ tests/RegExpUri.test.ts
@@ -1,7 +1,67 @@
+import {
+	assertExists,
+	assertObjectMatch,
+} from "https://deno.land/std@0.108.0/testing/asserts.ts"
 import RegExpUri from "../src/RegExpUri.ts"
 
-Deno.test("RegExpUri", async () => {
+Deno.test("RegExpUri/exact/never/never/always", async () => {
 	const { assertEquals } = await import("std/testing/asserts.ts")
-	// TODO: write tests
-	const re = new RegExpUri({ exact: true })
+	const re = new RegExpUri({
+		exact: true,
+		requireScheme: "never",
+		requireDoubleSlash: "never",
+		requireAuthority: "always",
+	})
+	const valid: string[] = [
+		"username:password@example.org:80/path?query#fragment",
+	]
+	const invalid: string[] = [""]
+	for (const target of valid) {
+		const result = target.match(re)?.groups
+		assertExists(result)
+		assertObjectMatch(result, {
+			uri: target,
+			authority: "username:password@example.org:80",
+			userinfo: "username:password",
+			host: "example.org",
+			port: "80",
+			path: "/path",
+			query: "query",
+			fragment: "fragment",
+		})
+	}
+	for (const target of invalid) {
+		assertEquals(target.match(re)?.groups, undefined)
+	}
+})
+
+Deno.test("RegExpUri/exact/undefined/undefined/undefined", async () => {
+	const { assertEquals } = await import("std/testing/asserts.ts")
+	const re = new RegExpUri({
+		exact: true,
+		requireScheme: undefined,
+		requireDoubleSlash: undefined,
+		requireAuthority: undefined,
+	})
+	const valid: string[] = [
+		"username:password@example.org:80/path?query#fragment",
+	]
+	const invalid: string[] = [""]
+	for (const target of valid) {
+		const result = target.match(re)?.groups
+		assertExists(result)
+		assertObjectMatch(result, {
+			uri: target,
+			authority: "username:password@example.org:80",
+			userinfo: "username:password",
+			host: "example.org",
+			port: "80",
+			path: "/path",
+			query: "query",
+			fragment: "fragment",
+		})
+	}
+	for (const target of invalid) {
+		assertEquals(target.match(re)?.groups, undefined)
+	}
 })
